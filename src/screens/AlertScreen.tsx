33182cf9c2a2ca313325c6f3839068633573cd8e
melannieanunez <melannie.nunez@boosmap.com>
2021-10-23 12:24:43 -0300

    Prompt implementation

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Alert, Button, View } from 'react-native';
+import prompt from 'react-native-prompt-android';
 import { HeaderTitle } from '../components';
 import styles from '../theme/AppTheme';
 
@@ -14,11 +15,23 @@ const AlertScreen = () => {
       { text: 'OK', onPress: () => console.log('OK Pressed') }
     ]);
 
+  const showPrompt = () =>
+    prompt(
+      'Esta seguro?',
+      'Mensaje',
+      (valor: string) => console.log('info', valor),
+      {
+        type: 'plain-text'
+      }
+    );
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alert" />
 
       <Button title="Mostrar alerta" onPress={showAlert} />
+      <View style={{ height: 10 }} />
+      <Button title="Mostrar prompt" onPress={showPrompt} />
     </View>
   );
 };
